6a73b6ca9eafdce7fac7ebce3352dd570af43b3d
Bharat <kadamb208@gmail.com>
2024-07-07 11:46:26 +0530

    add Foodie-Hamburger

---
+++ Foodie-Hamburger/script.js
@@ -0,0 +1,28 @@
+const hamburgerIcon = document.querySelector('.hamburger-menu-container')
+const headerContent = document.querySelector('.header-content')
+const closeIcon = document.querySelector('.close-icon')
+const nav = document.querySelector('nav')
+
+// const top=document.querySelector('.go-to-top a')
+
+hamburgerIcon.addEventListener('click', (e) => {
+    e.stopPropagation()
+    headerContent.classList.add('menu-open')
+})
+
+nav.addEventListener('click', (e) => {
+    e.stopPropagation()
+})
+
+closeIcon.addEventListener('click', () => {
+    headerContent.classList.remove('menu-open')
+})
+
+window.addEventListener('click', () => {
+    headerContent.classList.remove('menu-open')
+})
+
+// top.addEventListener('click',(e)=>{
+//     e.preventDefault();
+//     document.querySelector('#top').scrollIntoView({behavior:'smooth'});
+// })
